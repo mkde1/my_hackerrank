62c2753e37dc0a32d09e0b38ddccb351e50713a2
Kamil <mkd_git@yahoo.com>
2022-01-05 23:16:39 +0200

    Signed-off-by: Kamil <mkd_git@yahoo.com>

--- Printing Pattern Using Loops/main.c
+++ Printing Pattern Using Loops/main.c
@@ -9,13 +9,13 @@ int main()
     int n=0;
     scanf("%d", &n);
   	// Complete the code to print the pattern.
-    int i=0, j=0,m=0,sayac1=0;
-    if(n>=1){
+    int i=0, j=0,m=0,sayac1=0;//i for rows, j for columns, m changes with the row counter called "sayac1", the row counter "sayac1"
+    if(n>=1){//n should be between 1 or 1000(equal to)
         if(n<=1000){
-            for(i=0;i<((2*n)-1);i++){			//satirlar icin dongu
-                for(j=0;j<((2*n)-1);j++){		//sutunlar icin dongu
-                	if(i<n){					//i<n degerleri icin
-                		if(j<n){				//n degerinden kucuk sutun degerleri icin			
+            for(i=0;i<((2*n)-1);i++){			//loop for rows
+                for(j=0;j<((2*n)-1);j++){		//loop for columns
+                	if(i<n){					//rows belown "n"
+                		if(j<n){				//columns belown "n"			
                 			if(sayac1>m){
                 				printf("%d ",n-m);
                 				m++;
@@ -24,7 +24,7 @@ int main()
                 				printf("%d ",n-m);
                 			}
                 		}
-                		else{					//j>=n degerinden buyuk sutun degerleri icin
+                		else{					//columns above or equal to "n"
                 			if(((2*n)-2-j)<sayac1){
                 				m--;
                 				printf("%d ",n-m);
@@ -34,8 +34,8 @@ int main()
                 			}
                 		}
                 	}
-					else{						//i>n degerleri icin
-						if(j<n){				//n degerinden kucuk sutun degerleri icin
+					else{						//rows above or equal to "n"
+						if(j<n){				//columns below "n"
                 			if(sayac1>m){
                 				printf("%d ",n-m);
                 				m++;
@@ -44,7 +44,7 @@ int main()
                 				printf("%d ",n-m);
                 			}
                 		}
-                		else{					//j>=n degerinden buyuk sutun degerleri icin
+                		else{					//columns above or equal to "n"
                 			if(((2*n)-2-j)<sayac1){
                 				m--;
                 				printf("%d ",n-m);
@@ -55,9 +55,9 @@ int main()
                 		}
 					}				
                 }
-            printf("\n");
+            printf("\n");//prints new line after each row	
 			if(i<n-1){
-				sayac1++;
+				sayac1++;//row counter increases
 			}	
 			else{
 				sayac1--;
